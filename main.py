c7b65117e17d545cec635ab9d8ebd58bdafa94d5
n.moroshkin <n.moroshkin@cism-ms.ru>
2023-10-28 11:15:44 +0300

    add blip model for test (blip-vqa-capfilt-large)

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from pathlib import Path
 
 
-from src import logger
-
 from src.metrics.vqa_accuracy import VQAAccuracy
 from src.test_pipelines.vqa_accuracy import VQAAccuracyTestPipeline
 from src.pipelines.dummy import DummyPipeline
+from src.pipelines.blip import BlipPipeline
 from src.dataset_loaders.dummy import DummyDatasetLoader
 
 
 if __name__ == '__main__':
+    # TODO: Add argparse
     dummy_dataset = DummyDatasetLoader(path_to_data=Path('data', 'dummy'))
     dummy_dataset.load_data()
 
     dummy_metric = VQAAccuracy()
+
     dummy_pipeline = DummyPipeline()
+    blip_pipeline = BlipPipeline('Salesforce/blip-vqa-capfilt-large')
 
     dummy_test_pipeline = VQAAccuracyTestPipeline()
+
+    # create test pipeline
     dummy_test_pipeline.add_test_dataset(dummy_dataset)
     dummy_test_pipeline.add_metric(dummy_metric)
-    dummy_test_pipeline.add_model(dummy_pipeline)
+    dummy_test_pipeline.add_model(blip_pipeline)
 
+    # run tests
     dummy_test_pipeline.test()
---
+++ src/pipelines/blip.py
@@ -0,0 +1,30 @@
+from abc import ABC
+from typing import List
+
+import numpy as np
+from transformers import AutoProcessor, BlipForQuestionAnswering
+
+from src.pipelines import PipelineMeta
+
+
+class BlipPipeline(PipelineMeta, ABC):
+
+    __name__ = '__TYPE__'
+
+    def __init__(self, path_to_model):
+        self.processor = None
+        self.__name__ = self.__name__.replace('__TYPE__', path_to_model.split('/')[1])
+        super().__init__(path_to_model)
+
+    def load_model(self, path_to_model: str) -> None:
+        model = BlipForQuestionAnswering.from_pretrained(path_to_model)
+        self.processor = AutoProcessor.from_pretrained(path_to_model)
+        return model
+
+    def predict(self, image: np.array, batch_questions: np.array) -> List[np.array]:
+        batch_answers = []
+        for question in batch_questions:
+            inputs = self.processor(images=image, text=question, return_tensors="pt")
+            batch_answers.append(self.processor.decode(self.model.generate(**inputs)[0],
+                                                       skip_special_tokens=True))
+        return batch_answers
