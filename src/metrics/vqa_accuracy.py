7edd35e5b8db3e6e0cb21a5c0f6710daf44e8750
n.moroshkin <n.moroshkin@cism-ms.ru>
2023-10-28 11:02:56 +0300

    reformat code

--- src/metrics/vqa_accuracy.py
+++ src/metrics/vqa_accuracy.py
@@ -9,52 +9,52 @@ from src.metrics import MetricMeta
 
 
 class VQAAccuracy(MetricMeta, ABC):
-
     __name__ = 'VQA Accuracy'
 
     _allow_types = ['vqa', 'classic']
 
     _contractions = {"aint": "ain't", "arent": "aren't", "cant": "can't", "couldve": "could've",
-                      "couldnt": "couldn't",
+                     "couldnt": "couldn't",
                      "couldn'tve": "couldn't've", "couldnt've": "couldn't've", "didnt": "didn't",
-                      "doesnt": "doesn't", "dont": "don't", "hadnt": "hadn't",
-                     "hadnt've": "hadn't've", "hadn'tve": "hadn't've", "hasnt": "hasn't", "havent": "haven't","hed": "he'd", "hed've": "he'd've",
+                     "doesnt": "doesn't", "dont": "don't", "hadnt": "hadn't",
+                     "hadnt've": "hadn't've", "hadn'tve": "hadn't've", "hasnt": "hasn't", "havent": "haven't",
+                     "hed": "he'd", "hed've": "he'd've",
                      "he'dve": "he'd've", "hes": "he's", "howd": "how'd", "howll": "how'll", "hows": "how's",
-                      "Id've": "I'd've", "I'dve": "I'd've",
+                     "Id've": "I'd've", "I'dve": "I'd've",
                      "Im": "I'm", "Ive": "I've", "isnt": "isn't", "itd": "it'd", "itd've": "it'd've",
-                      "it'dve": "it'd've", "itll": "it'll", "let's": "let's",
+                     "it'dve": "it'd've", "itll": "it'll", "let's": "let's",
                      "maam": "ma'am", "mightnt": "mightn't", "mightnt've": "mightn't've",
-                      "mightn'tve": "mightn't've", "mightve": "might've",
+                     "mightn'tve": "mightn't've", "mightve": "might've",
                      "mustnt": "mustn't", "mustve": "must've", "neednt": "needn't", "notve": "not've",
-                      "oclock": "o'clock", "oughtnt": "oughtn't",
+                     "oclock": "o'clock", "oughtnt": "oughtn't",
                      "ow's'at": "'ow's'at", "'ows'at": "'ow's'at", "'ow'sat": "'ow's'at", "shant": "shan't",
-                      "shed've": "she'd've", "she'dve": "she'd've",
+                     "shed've": "she'd've", "she'dve": "she'd've",
                      "she's": "she's", "shouldve": "should've", "shouldnt": "shouldn't",
-                      "shouldnt've": "shouldn't've", "shouldn'tve": "shouldn't've",
+                     "shouldnt've": "shouldn't've", "shouldn'tve": "shouldn't've",
                      "somebody'd": "somebodyd", "somebodyd've": "somebody'd've", "somebody'dve": "somebody'd've",
-                      "somebodyll": "somebody'll",
+                     "somebodyll": "somebody'll",
                      "somebodys": "somebody's", "someoned": "someone'd", "someoned've": "someone'd've",
-                      "someone'dve": "someone'd've",
+                     "someone'dve": "someone'd've",
                      "someonell": "someone'll", "someones": "someone's", "somethingd": "something'd",
-                      "somethingd've": "something'd've",
+                     "somethingd've": "something'd've",
                      "something'dve": "something'd've", "somethingll": "something'll", "thats": "that's",
-                      "thered": "there'd", "thered've": "there'd've",
+                     "thered": "there'd", "thered've": "there'd've",
                      "there'dve": "there'd've", "therere": "there're", "theres": "there's", "theyd": "they'd",
-                      "theyd've": "they'd've",
+                     "theyd've": "they'd've",
                      "they'dve": "they'd've", "theyll": "they'll", "theyre": "they're", "theyve": "they've",
-                      "twas": "'twas", "wasnt": "wasn't",
+                     "twas": "'twas", "wasnt": "wasn't",
                      "wed've": "we'd've", "we'dve": "we'd've", "weve": "we've", "werent": "weren't",
-                      "whatll": "what'll", "whatre": "what're",
+                     "whatll": "what'll", "whatre": "what're",
                      "whats": "what's", "whatve": "what've", "whens": "when's", "whered": "where'd",
-                      "wheres": "where's", "whereve": "where've",
+                     "wheres": "where's", "whereve": "where've",
                      "whod": "who'd", "whod've": "who'd've", "who'dve": "who'd've", "wholl": "who'll",
-                      "whos": "who's", "whove": "who've", "whyll": "why'll",
+                     "whos": "who's", "whove": "who've", "whyll": "why'll",
                      "whyre": "why're", "whys": "why's", "wont": "won't", "wouldve": "would've",
-                      "wouldnt": "wouldn't", "wouldnt've": "wouldn't've",
+                     "wouldnt": "wouldn't", "wouldnt've": "wouldn't've",
                      "wouldn'tve": "wouldn't've", "yall": "y'all", "yall'll": "y'all'll", "y'allll": "y'all'll",
-                      "yall'd've": "y'all'd've",
+                     "yall'd've": "y'all'd've",
                      "y'alld've": "y'all'd've", "y'all'dve": "y'all'd've", "youd": "you'd", "youd've": "you'd've",
-                      "you'dve": "you'd've",
+                     "you'dve": "you'd've",
                      "youll": "you'll", "youre": "you're", "youve": "you've"}
 
     _articles = ['a', 'an', 'the']
@@ -62,8 +62,8 @@ class VQAAccuracy(MetricMeta, ABC):
     _period_strip = re.compile(r"(\d)(\.)(\d)")
     _comma_strip = re.compile(r"(\d)(\,)(\d)")
     _punct = [';', r"/", '[', ']', '"', '{', '}',
-               '(', ')', '=', '+', '\\', '_', '-',
-               '>', '<', '@', '`', ',', '?', '!']
+              '(', ')', '=', '+', '\\', '_', '-',
+              '>', '<', '@', '`', ',', '?', '!']
 
     def _preprocess_sentence(self, sentence):
         sentence_ = []
@@ -101,7 +101,6 @@ class VQAAccuracy(MetricMeta, ABC):
         for vqa_unit in vqa_units:
 
             for answer, model_answer in zip(vqa_unit['answers'], vqa_unit['model_answers']):
-
                 answer_ = self._preprocess_sentence(answer)
                 model_answer_ = self._preprocess_sentence(model_answer)
 
